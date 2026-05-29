49cd94b6fa47e572d4926a0dc7217a84b6c605a4
EstherAzevedo <estherazvdo123@gmail.com>
2026-05-29 12:51:02 -0300

    feat: início de dashboard.php

--- actions/login_action.php
+++ actions/login_action.php
@@ -62,7 +62,7 @@ $_SESSION['user_id'] = $usuario['id'];
 $_SESSION['user_name'] = $usuario['nome'];
 $_SESSION['user_email'] = $usuario['email'];
 
-header("Location: ../index.php");
+header("Location: ../pages/dashboard.php");
 exit();    
 
 ?>
