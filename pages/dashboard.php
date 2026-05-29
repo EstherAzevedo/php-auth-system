4597cb9526fa1d21ba02d601000cac2ebefc2b86
EstherAzevedo <estherazvdo123@gmail.com>
2026-05-29 15:39:19 -0300

    feat: dashboard protegida

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,11 +1,31 @@
+<?php
+
+require_once("../includes/auth.php");
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>dashboard</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
+
 <body>
-    <h1>Dashboard aqui!</h1>
+    <div class="container-dashboard">
+        <h1>
+            Bem vindo,
+            <?= htmlspecialchars($_SESSION['user_name']); ?>
+        </h1>
+
+        <a href="../actions/logout_action.php">
+            Sair
+        </a>
+
+    </div>
 </body>
+
 </html>
